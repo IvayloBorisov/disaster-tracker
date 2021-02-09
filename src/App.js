34ee202a1c05473e2082dd4b22a2346868475ac2
IvayloBorisov <ivaylo_borisov@abv.bg>
2021-02-09 22:32:54 +0200

    Add Popup component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const App = () => {
       setEventData(events.flat());
       setLoading(false);
     }
-
     getData();
 
   }, []);
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,34 +1,32 @@
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
-import { LocationMarker } from '../index';
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+import { LocationMarker, PopupComponent } from "../index";
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const MapComponent = ({ eventData }) => {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 39.7837304,
+    longitude: -100.4458825,
+    zoom: 4,
+  });
 
-    const [ viewport, setViewport ] = useState({
-        width: '100vw',
-        height: '100vh',
-        latitude: 51,
-        longitude: 5,
-        zoom: 1,
-        position: 'absolute',
-        top: '0',
-        left: '0'
-    });
+  
 
-    return(
-        <div>
-            <ReactMapGL
-             {...viewport}
-             mapboxApiAccessToken= { MAPBOX_TOKEN }
-             mapStyle= "mapbox://styles/ivoborisov/ckktptabb2du117qkyo4jg5tq"
-             onViewportChange= { (viewport) => setViewport(viewport) }
-            >
-                { eventData.map(event => <LocationMarker key={ event.id } {...event}/>) }
-            </ReactMapGL>
-        </div>
-    )
-}
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/ivoborisov/ckktptabb2du117qkyo4jg5tq"
+        onViewportChange={(viewport) => setViewport(viewport)}
+      >
+        {eventData.map(event => <LocationMarker key={event.id} {...event} />)}
+      </ReactMapGL>
+    </div>
+  );
+};
 
 export default MapComponent;
